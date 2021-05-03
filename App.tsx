14808946a96f668fcb4f76da4fdb97ddddf77b99
Tay <taynara.nogueira@summit-bra.com>
2021-05-03 17:26:42 -0300

    plantinha

---
+++ App.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import {  } from "react-native";
+import { Welcome } from "./src/pages/welcome";
+
+
+export default function App(){
+  return(
+    <>
+
+        <Welcome/>
+
+    </>
+  )
+}
+
+
