8a3564d2d27b8e35750691c5e1ca4d1c80430022
Tay <taynara.nogueira@summit-bra.com>
2021-05-03 17:41:11 -0300

    Atualizaçao

--- src/pages/welcome.tsx
+++ src/pages/welcome.tsx
@@ -1,13 +1,28 @@
 import React from "react"
-import { View, Text, Image } from "react-native"
+import styled from "styled-components/native"
+import { SafeAreaView, Text, Image, StyleSheet } from "react-native"
 
 import waterringImg from '../img/waterdrop.png'
 export function Welcome(){
     return(
-        <View>
+        <Container>
             <Text>Gerencie suas plantas de forma fácil!</Text>
             <Image source= {waterringImg}/>
 
-        </View>
+        </Container>
     )
 }
+
+// const style = StyleSheet.create({
+//     container:{
+//         flex:1,
+//         justifyContent:"center",
+//         alignItems: "center",
+//     }
+// })
+
+const Container = styled.SafeAreaView `
+    flex:1;
+    justify-content: center;
+    align-items: center;
+`
