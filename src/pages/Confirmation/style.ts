4835c3ceb73fbd9aaf8398ff40aae9af618ed839
Tay <taynara.nogueira@summit-bra.com>
2021-05-05 17:53:30 -0300

    Tela de confirmação (Inicio)

---
+++ src/pages/Confirmation/style.ts
@@ -0,0 +1,23 @@
+import styled from "styled-components/native"
+import Color from "../../../styles/colors"
+import fonts from "../../../styles/fontes"
+
+
+export const Container = styled.SafeAreaView `
+    flex:1;
+    width: 100%;
+    justify-content: space-around;
+    align-items: center;
+`
+
+
+export const Wrapper= styled.View`
+    flex: 1;
+    width: 100%;
+`
+
+export const Background= styled.ImageBackground`
+    flex: 1;
+    width: 100%;
+`
+
